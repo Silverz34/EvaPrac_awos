a8530182fafd0600875e4f4e9b973de2cef228a3
Silverz34 <pradogordillomichael@gmail.com>
2026-02-05 00:13:07 -0600

    si me costo la 3, aun no se si deba redondear

--- dashboard_student/db/reports_vw.sql
+++ dashboard_student/db/reports_vw.sql
@@ -42,6 +42,7 @@ tuvieron al menos 1 alumno COUNT(e.id) > 0
 -- VIEW
 CREATE VIEW vw_teacher_load AS
 SELECT 
+    t.id AS id,
     t.name AS nombre
     g.term AS periodo
     COUNT(DISTINCT g.id) AS total_grupos,
@@ -53,4 +54,49 @@ LEFT JOIN enrollments e ON g.id = e.group_id
 LEFT JOIN grades gr ON e.id = gr.enrollment_id
 GROUP BY t.id, t.name, g.term
 HAVING COUNT(e.id) > 0;
+
+-- VERIFY:
+SELECT * FROM vw_teacher_load ORDER BY nombre, total_grupos;
+
+------------------------------------------------------------------------------------------------------
+/*
+VIEW 3: vw_students_at_risk 
+REPORTE 3: alumnos en riesgo académico
+Qué devuelve: casos distintos de riesgo académico 
+Grain (una fila representa): un alumno
+Métrica(s): promedio general AVG(COALESCE(gr.final, 0)), asistencia baja AVG(CASE WHEN a.present) )
+Por qué GROUP BY / HAVING / subconsulta: CASE para mostrar mensajes, dependiendo la validacion.
+WHERE para filtar alumnos con promedio < 6.0 o asistencia < 75%
+*/
+
+-- VIEW
+CREATE VIEW vw_students_at_risk AS
+WITH estudiante_status AS (
+    SELECT 
+        s.id AS id,
+        s.name AS nombre_alumno,
+        s.email AS correo,
+        s.program AS licenciatura,
+        AVG(COALESCE(gr.final, 0)) AS promedio_general,
+        AVG(CASE WHEN a.present THEN 100 ELSE 0 END) AS asistencia
+    FROM students s
+    JOIN enrollments e ON s.id = e.student_id
+    LEFT JOIN grades gr ON e.id = gr.enrollment_id
+    LEFT JOIN attendance a ON e.id = a.enrollment_id
+    GROUP BY s.id, s.name, s.email, s.program
+)
+SELECT 
+    id,
+    nombre_alumno,
+    correo,
+    licenciatura,
+    --ROUND(promedio_general, 2) AS current_avg,
+    --ROUND(asistencia, 2) AS attendance_pct,
+    CASE 
+        WHEN promedio_general < 6.0 AND asistencia < 75 THEN 'CRITICO: Académico y Asistencia'
+        WHEN promedio_general < 6.0 THEN 'Riesgo Académico'
+        ELSE 'Riesgo Asistencia'
+    END AS riesgo_academico
+FROM estudiante_status
+WHERE promedio_general < 6.0 OR asistencia< 75;
 
